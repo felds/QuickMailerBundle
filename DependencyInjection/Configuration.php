834a0f3483b3a45ec94f2f36c7d276765f12affb
Felds Liscia <dev@felds.com.br>
2018-02-01 16:13:42 -0200

    add logger service config

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -46,6 +46,7 @@ class Configuration implements ConfigurationInterface
                         ->end()
                     ->end()
                 ->end() // templates
+                ->scalarNode('logger_service')->defaultValue('logger')->end()
             ->end()
         ;
 
