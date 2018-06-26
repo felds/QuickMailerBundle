f2dc390dbca63c4980be8e368a81c3810cce292d
Felds Liscia <dev@felds.com.br>
2018-06-26 17:02:54 -0300

    change Template class to send own email

--- Template.php
+++ Template.php
@@ -3,42 +3,155 @@ declare(strict_types=1);
 
 namespace Felds\QuickMailerBundle;
 
-use Felds\QuickMailerBundle\Model\MailableInterface;
+use Felds\QuickMailerBundle\Model\Mailable;
+use Psr\Log\LoggerInterface;
+use Swift_Mailer;
+use Throwable;
 use Twig_Environment;
 use Twig_TemplateWrapper;
 
 class Template
 {
     /**
-     * @var QuickMailerInterface
+     * @var string
      */
-    private $quickMailer;
+    private $name;
 
     /**
-     * @var array<string, mixed>
+     * @var Twig_Environment
+     */
+    private $twig;
+
+    /**
+     * @var Swift_Mailer
+     */
+    private $mailer;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var array<string, array<string, mixed>> config
      */
     private $config;
 
     /**
-     * @var bool
+     * @var Mailable
      */
-    private $isEnabled;
+    private $from;
+
+    /**
+     * @var Mailable|null
+     */
+    private $replyTo;
 
     /**
      * @var Twig_TemplateWrapper
      */
-    private $twigTemplate;
+    private $templateWrapper;
 
-    public function __construct(QuickMailerInterface $quickMailer, Twig_Environment $twig, array $config)
-    {
-        $this->quickMailer = $quickMailer;
+    /**
+     * @param
+     */
+    public function __construct(
+        string $name,
+        array $config,
+        Twig_Environment $twig,
+        Swift_Mailer $mailer,
+        LoggerInterface $logger,
+        Mailable $from,
+        ?Mailable $replyTo = null
+    ) {
+        $this->name = $name;
         $this->config = $config;
-        $this->isEnabled = $config['enabled'];
-        $this->twigTemplate = $twig->load($config['path']);
+        $this->twig = $twig;
+        $this->mailer = $mailer;
+        $this->logger = $logger;
+        $this->from = $from;
+        $this->replyTo = $replyTo;
+        $this->templateWrapper = $twig->load($config['path']);
     }
 
-    public function sendTo(MailableInterface $recipient, array $payload = []): int
+    /**
+     * @param Mailable $recipient Who should receive the email
+     * @param array<string, mixed> $payload Data to be used by the template.
+     * @return int Number of emails sent (usually 1 on success; 0 on failure)
+     */
+    public function sendTo(Mailable $recipient, array $payload = []): int
     {
-        return -1;
+        if (!$this->config['enabled']) {
+            $this->logger->notice("The quickmailer {$this->name} is disabled.");
+
+            return 0;
+        }
+
+        $subject = $this->getSubject($payload);
+        $htmlBody = $this->getPart('html', $payload);
+        $textBody = $this->getPart('text', $payload);
+
+        if (!$subject) {
+            throw new \RuntimeException("A subject is required. Please add a non-empty `subject` block in the template {$this->config['path']}.");
+        }
+
+        if (!($htmlBody || $textBody)) {
+            throw new \RuntimeException("The template {$this->config['path']} should have either an `html` block or a `text` block. Preferably both.");
+        }
+
+        try {
+            /** @var \Swift_Message $message */
+            $message = $this->mailer->createMessage()
+                ->setFrom([$this->from->getEmail() => $this->from->getName()])
+                ->setTo([$recipient->getEmail() => $recipient->getName()])
+                ->setSubject($subject)
+            ;
+
+            if ($this->replyTo) {
+                $message->setReplyTo([$this->replyTo->getEmail() => $this->replyTo->getName()]);
+            }
+
+            if ($htmlBody) {
+                $message->addPart($htmlBody, 'text/html');
+            }
+
+            if ($textBody) {
+                $message->addPart($textBody, 'text/plain');
+            }
+
+            $this->logger->info("Sending email...", [
+                'id' => $message->getId(),
+                'name' => $this->name,
+                'from' => $this->from ? [$this->from->getName(), $this->from->getEmail()] : null,
+                'reply_to' => $this->replyTo ? [$this->replyTo->getName(), $this->replyTo->getEmail()] : null,
+                'to' => $recipient ? [$recipient->getName(), $recipient->getEmail()] : null,
+            ]);
+
+            return $this->mailer->send($message);
+        } catch (Throwable $exception) {
+            $this->logger->critical("The email cannot be sent!", [
+                'message' => $exception->getMessage(),
+                'name' => $this->name,
+                'from' => $this->from ? [$this->from->getName(), $this->from->getEmail()] : null,
+                'reply_to' => $this->replyTo ? [$this->replyTo->getName(), $this->replyTo->getEmail()] : null,
+                'to' => $recipient ? [$recipient->getName(), $recipient->getEmail()] : null,
+            ]);
+        }
+
+        return 0;
+    }
+
+    private function getSubject(array $payload): string
+    {
+        return trim((string)$this->getPart('subject', $payload));
+    }
+
+    private function getPart(string $blockName, $payload): ?string
+    {
+        if (in_array($blockName, $this->templateWrapper->getBlockNames())) {
+            return $this->templateWrapper->renderBlock($blockName, $payload);
+        } else {
+            return null;
+        }
     }
 }
